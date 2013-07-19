cff173be683eec21cac69d03fefaac0de955bb5c
Anupam Mittal <anuapmm@yahoo.com>
2013-07-19 13:27:18 +0530

    testing

--- web.js
+++ web.js
@@ -9,7 +9,6 @@ app.get('/', function (request, response) {
     fs.readFile('index.html', function (err, data) {
         if (err) throw err;
         response.send(data.toString());
- 
     });
 
 });
@@ -23,3 +22,4 @@ app.get('/Quote', function (request, response) {
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
+});
