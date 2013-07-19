8172b880ce62e5090a79a4476ba8f463449aedd5
Anupam Mittal <anuapmm@yahoo.com>
2013-07-19 14:36:40 +0530

    csv

--- web.js
+++ web.js
@@ -18,7 +18,29 @@ app.get('/', function (request, response) {
 
 
 app.get('/Quote', function (request, response) {
-    response.send('wip');
+            var finalCSVString = "";
+            var timeNow = moment();
+
+            var fs = require('fs');
+            var symbolsArray = fs.readFileSync('futureslist.tls').toString().split("\r\n");
+
+            async.forEach(symbolsArray,
+            function (symbol, callback) {
+                //console.log("!!!" + symbol);
+                iciciquote.returnQuoteJSON(symbol, function (foo) {
+                    finalCSVString += foo.Symbol + "," + (moment(foo.LastTradeDate, 'DD-MMM-YYYY')).format('YYYYMMDD') + "," + foo.DayOpen + "," + foo.DayHigh + "," + foo.DayLow + "," + foo.LastTradePrice + "," + foo.Volume + "," + foo.PrevDayClose + "\r\n";
+                    callback();
+                });
+            },
+             function () {
+                 //var jsonString = JSON.stringify(foo);
+                 //console.log("..." + finalCSVString);
+                 //fs.writeFile("test.csv", finalCSVString);
+                 //response.send(test.csv);
+                 response.send(finalCSVString);
+                 //console.log("took " + moment().diff(timeNow));
+             }
+            );
 
 });
 
